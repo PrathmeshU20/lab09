a2efc6e0807450437ecb4261dc3ea2778383dac6
PrathmeshU20 <prathmeshurkude20@gmail.com>
2023-12-25 19:57:42 +0530

    Add admin middleware

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::get('/admin', [AdminController::class, 'index'])->middleware('auth')->name('admin');
 
@@ -41,16 +40,16 @@ Route::get('/admin/users', [
 
 Route::get('/admin/users/create', [
     AdminUsersController::class, 'create'
-])->middleware('auth')->name('admin-users-create');
+])->middleware('admin')->name('admin-users-create');
 
 Route::post('/admin/users/create', [
     AdminUsersController::class, 'store'
-])->middleware('auth')->name('admin-users-create');
+])->middleware('admin')->name('admin-users-create');
 
 Route::get('/admin/users/{id}/edit', [
     AdminUsersController::class, 'edit'
-])->middleware('auth')->name('admin-users-edit');
+])->middleware('admin')->name('admin-users-edit');
 
 Route::patch('/admin/users/{id}/edit', [
     AdminUsersController::class, 'update'
-])->middleware('auth')->name('admin-users-edit');
+])->middleware('admin')->name('admin-users-edit');
